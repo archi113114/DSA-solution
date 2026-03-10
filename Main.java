e68ef35b4d58d77381344780945174b53e498bb8
archi113114 <architasingh1216@gmail.com>
2026-03-10 12:21:04 +0530

    change

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main {
+  
+//  Question --1
+    public static boolean isPrime(int n) {
+      if (n <= 1)
+         return false;
+      for (int i = 2; i < n; i++) {
+        if (n % i == 0)
+             return false;
+      }
+      return true;
+    }
+
+    public static void Triplets(int limit){
+        for (int a = 1; a < limit; a++) {
+            for (int b = a; b < limit; b++) {
+                int c = (int) Math.sqrt(a * a + b * b);
+                if (c < limit && a * a + b * b == c * c) {
+                    System.out.println(a + " " + b + " " + c);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int n = 29;
+        System.out.println(isPrime(n));
+        Triplets(20);
+    }
+}
+
